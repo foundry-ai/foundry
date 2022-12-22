dad03f096ae3c54cae8ef8e6b37bbadbd1030c1e
Daniel Pfrommer <dan.pfrommer@gmail.com>
2022-12-21 23:06:14 -0500

    Removing old project setup

--- docker/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-PROJECT=ilc
-
-DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cp $DIR/../requirements.txt $DIR/requirements.txt.tmp
-docker build -t $USER-$PROJECT-container $DIR
--- tests/dataset.py
+++
@@ -1,6 +0,0 @@
-# First fix the imports
-import os
-import sys
-if __name__=="__main__":
-    sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..')))
-    sys.path.pop(0)
