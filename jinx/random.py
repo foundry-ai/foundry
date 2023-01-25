8922df3b620bf2fbdd423d1312a249c11dd930c8
Stephen Tu <tu.stephenl@gmail.com>
2023-01-25 12:27:22 -0500

    return key_or_seed

--- jinx/random.py
+++ jinx/random.py
@@ -11,6 +11,7 @@ def key_or_seed(key_or_seed):
     elif (hasattr(key_or_seed, "shape") and (not key_or_seed.shape) and
             hasattr(key_or_seed, "dtype") and key_or_seed.dtype == jnp.int32):
         key_or_seed = jax.random.PRNGKey(key_or_seed)
+    return key_or_seed
 
 class PRNGSequence:
     def __init__(self, key_or_val):
