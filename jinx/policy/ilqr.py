88ca8cdd6e9cf6e4836648c609195a81a8fd471a
dan.pfrommer@gmail.com <dan.pfrommer@gmail.com>
2023-01-26 01:56:25 -0500

    fixed stupid receeding thing

--- jinx/policy/ilqr.py
+++ jinx/policy/ilqr.py
@@ -20,15 +20,38 @@ class ILQR:
         self.receed = receed
         self.verbose = verbose
     
-    def _solve(self, x0, init_us):
+    def _solve(self, x0, init_us, curr_T):
         x_vec, _ = jax.flatten_util.ravel_pytree(x0)
+
+        def ilqr_cost(x, u, t):
+            if self.receed:
+                t = t + curr_T
+                cost = jax.lax.cond(t > self.horizon_length,
+                    lambda: 0.,
+                    lambda: 
+                        jax.lax.cond(t == self.horizon_length,
+                            lambda: self.cost_fn(x, None),
+                            lambda: self.cost_fn(x, u))
+                )
+                return cost
+            else:
+                return jax.lax.cond(t == self.horizon_length,
+                    lambda: self.cost_fn(x, None),
+                    lambda: self.cost_fn(x, u))
+
         _, us, _, grad, _, _, iters = trajax.optimizers.ilqr(
-            lambda x,u,t: jax.lax.cond(t == self.horizon_length,
-                lambda: self.cost_fn(x, None),
-                lambda: self.cost_fn(x, u)),
+            ilqr_cost,
             lambda x,u,t: self.model_fn(x, u), 
             x_vec, init_us
         )
+
+        # _, us, _, grad, _, _, iters = trajax.optimizers.ilqr(
+        #     lambda x,u,t: jax.lax.cond(t == self.horizon_length,
+        #         lambda: self.cost_fn(x, None),
+        #         lambda: self.cost_fn(x, u)),
+        #     lambda x,u,t: self.model_fn(x, u), 
+        #     x_vec, init_us
+        # )
         if self.verbose:
             logger.info('ilqr grad norm {}, iters {}',
                         jnp.linalg.norm(jax.flatten_util.ravel_pytree(grad)[0]),
@@ -38,7 +61,7 @@ class ILQR:
     def init_state(self, x0):
         u_vec, _ = jax.flatten_util.ravel_pytree(self.u_sample)
         us = jnp.zeros((self.horizon_length - 1,) + u_vec.shape)
-        us = self._solve(x0, us)
+        us = self._solve(x0, us, 0)
         return us, 0
     
     @partial(jax.jit, static_argnums=(0,))
@@ -49,7 +72,7 @@ class ILQR:
         if self.receed:
             us = us.at[:-1].set(us[1:])
             # re-solve
-            us = self._solve(state, us)
+            us = self._solve(state, us, T)
             return u_unflatten(us[0]), (us, T+1)
         else:
             return u_unflatten(us[T]), (us, T+1)
