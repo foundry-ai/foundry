bd49d38614fd2b56d849ff3bf761538518f7080b
Daniel Pfrommer <dan.pfrommer@gmail.com>
2023-09-11 19:16:15 -0400

    Added quadrotor MLP with cost rollouts notebook

--- stanza/train/validate.py
+++ stanza/train/validate.py
@@ -14,20 +14,33 @@ class Validator:
     dataset: Data
     condition : Callable = every_epoch
     stat_fn : Callable = None
+    stat_key : str = field(default="test", jax_static=True)
     batch_size: int = field(default=None, jax_static=True)
     # set to None to run through the entire validation dataset
     samples_per_run: int = field(default=None)
 
     def init(self, state):
-        state = replace(
-            state,
-            last_stats=replace(state.last_stats,
-                test=state.last_stats["train"])
-        )
+        sample = self.dataset[0]
+        batch = jax.tree_map(lambda x: jnp.expand_dims(x, 0), sample)
+
+        if self.stat_fn is None:
+            _, _, stats = state.config.loss_fn(
+                state.fn_state,
+                state.fn_params, 
+                state.rng_key, batch
+            )
+        else:
+            stats = self.stat_fn(state.fn_state, 
+                    state.fn_params, state.rng_key, batch)
+        last_stats = dict(state.last_stats)
+        last_stats[self.stat_key] = stats
+        state = replace(state, last_stats=last_stats)
         return (self.rng_key, state.iteration), state
 
     def __call__(self, hs, state):
         _, iteration = hs
+        stat_fn = self.stat_fn if self.stat_fn is not None else \
+            lambda s, p, r, b: state.config.loss_fn(s,p,r,b)[2]
         def cond_fn(hs, state):
             rng_key, _ = hs
             rng_key, sk = jax.random.split(rng_key)
@@ -38,15 +51,11 @@ class Validator:
                 def scan_fn(carry, batch):
                     batch = PyTreeData.from_data(batch).data
                     running_stats, total = carry
-                    if self.stat_fn is not None:
-                        _, _, stats = state.config.loss_fn(
-                            state.fn_state,
-                            state.fn_params, 
-                            rng_key, batch
-                        )
-                    else:
-                        stats = self.stat_fn(state.fn_state, 
-                                state.fn_params, rng_key, batch)
+                    stats = stat_fn(
+                        state.fn_state,
+                        state.fn_params, 
+                        rng_key, batch
+                    )
                     new_total = total + self.batch_size
                     running_stats = jax.tree_util.tree_map(
                         lambda x, y: x*(total/new_total) + y*(self.batch_size/new_total),
@@ -58,15 +67,17 @@ class Validator:
                 stats, _ = dataset.scan(scan_fn, (state.last_stats["test"], 0), limit=batches)
             else:
                 data = PyTreeData.from_data(self.dataset).data
-                _, _, stats = state.config.loss_fn(
-                    state.fn_state, state.fn_params,
-                    rng_key, data)
-                stats = jax.tree_util.tree_map(lambda x: jnp.mean(x), stats)
+                stats = stat_fn(
+                    state.fn_state,
+                    state.fn_params, 
+                    rng_key, data
+                )
             chex.assert_trees_all_equal_shapes_and_dtypes(
-                state.last_stats["test"], stats)
-            state = replace(
-                state, last_stats=replace(state.last_stats, test=stats)
-            )
+                state.last_stats[self.stat_key], stats)
+            
+            last_stats = dict(state.last_stats)
+            last_stats[self.stat_key] = stats
+            state = replace(state, last_stats=last_stats)
             return (rng_key, state.iteration), state
         return jax.lax.cond(jnp.logical_and(self.condition(state),
                             iteration != state.iteration),
