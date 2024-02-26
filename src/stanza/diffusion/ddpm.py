8c6a9f1c6927a582b9ee0a6a35dd5107096eed88
Daniel Pfrommer <dan.pfrommer@gmail.com>
2024-02-26 11:32:35 -0500

    Added gt score function computation

--- src/stanza/diffusion/ddpm.py
+++ src/stanza/diffusion/ddpm.py
@@ -11,6 +11,7 @@ class DDPMSchedule:
     betas: jnp.array
     alphas: jnp.array = struct.field(initializer=lambda x: 1  - x.betas)
     alphas_cumprod: jnp.array = struct.field(initializer=lambda x: jnp.cumprod(x.alphas))
+    alphas_cumprod_prev: jnp.array = struct.field(initializer=lambda x: jnp.concatenate((jnp.ones((1,)), x.alphas_cumprod[:-1]), axis=-1))
     variance_type: str = struct.field(default="fixed_small", pytree_node=False)
     # "epsilon", "sample", or "v_prediction"
     prediction_type: str = struct.field(default="epsilon", pytree_node=False)
@@ -39,6 +40,23 @@ class DDPMSchedule:
     def num_steps(self):
         return self.betas.shape[0]
 
+    @jax.jit
+    def forward_trajectory(self, rng_key, sample):
+        sample_flat, unflatten = jax.flatten_util.ravel_pytree(sample)
+        unfaltten_vmap = jax.vmap(unflatten)
+
+        noise_flat = jax.random.normal(rng_key, (self.num_steps,) + sample_flat.shape)
+        alphas_shifted = jnp.concatenate((jnp.ones((1,)), self.alphas_cumprod[:-1]), axis=-1)
+        noise_scales = alphas_shifted * self.betas
+
+        noise_flat = noise_flat * noise_scales[:,None]
+        noise_flat = jnp.cumsum(noise_flat, axis=0)
+        noisy_flat = noise_flat + self.alphas_cumprod[:,None]*sample_flat[None,:]
+
+        noise = unfaltten_vmap(noise_flat)
+        noisy = unfaltten_vmap(noisy_flat)
+        return noisy, noise
+
     # This will do the noising
     # forward process
     # will return noisy_sample, noise_eps, model_output 
@@ -59,29 +77,10 @@ class DDPMSchedule:
             return noisy, noise, sample
         else:
             raise ValueError("Not supported prediction type")
-    
-    @jax.jit
-    def noise_trajectory(self, rng_key, sample):
-        sample_flat, unflatten = jax.flatten_util.ravel_pytree(sample)
-        unfaltten_vmap = jax.vmap(unflatten)
-
-        noise_flat = jax.random.normal(rng_key, (self.num_steps,) + sample_flat.shape)
-        alphas_shifted = jnp.concatenate((jnp.ones((1,)), self.alphas_cumprod[:-1]), axis=-1)
-        noise_scales = alphas_shifted * self.betas
-
-        noise_flat = noise_flat * noise_scales[:,None]
-        noise_flat = jnp.cumsum(noise_flat, axis=0)
-        noisy_flat = noise_flat + self.alphas_cumprod[:,None]*sample_flat[None,:]
-
-        noise = unfaltten_vmap(noise_flat)
-        noisy = unfaltten_vmap(noisy_flat)
-        return noisy, noise
-
     @jax.jit
     def add_sub_noise(self, rng_key, sub_sample, sub_timestep, timestep):
         alphas_shifted = jnp.concatenate((jnp.ones((1,)), self.alphas_cumprod), axis=-1)
         alphas_prod = self.alphas_cumprod[timestep] / alphas_shifted[sub_timestep]
-
         sqrt_alphas_prod = jnp.sqrt(alphas_prod)
         sqrt_one_minus_alphas_prod = jnp.sqrt(1 - alphas_prod)
         sample_flat, unflatten = jax.flatten_util.ravel_pytree(sub_sample)
@@ -98,17 +97,70 @@ class DDPMSchedule:
             return noisy, noise, scaled_noise
         elif self.prediction_type == "sample":
             return noisy, noise, sub_sample
+        else:
+            raise ValueError("Not supported prediction type")
     
+    # returns E[x_0 | model_output, current sample]
+    @jax.jit
+    def denoised_from_output(self, noised_sample, t, model_output):
+        """ Returns E[x_0 | x_t] as computed by the model_output
+        based on the value of ``prediction_type``
+        """
+        alpha_prod_t = self.alphas_cumprod[t]
+        beta_prod_t = 1 - alpha_prod_t
+        sample_flat, unflatten = jax.flatten_util.ravel_pytree(noised_sample)
+        model_output_flat, _ = jax.flatten_util.ravel_pytree(model_output)
+        if self.prediction_type == "epsilon":
+            pred_sample = (sample_flat - beta_prod_t ** (0.5) * model_output_flat) / alpha_prod_t ** (0.5)
+        elif self.prediction_type == "sample":
+            pred_sample = model_output_flat
+        else:
+            raise ValueError("Not supported prediction type")
+
+        if self.clip_sample_range is not None:
+            pred_sample = jnp.clip(pred_sample, -self.clip_sample_range, self.clip_sample_range)
+        return unflatten(pred_sample)
+    
+    @jax.jit
+    def output_from_denoised(self, noised_sample, t, denoised_sample):
+        if self.prediction_type == "sample":
+            return denoised_sample
+        elif self.prediction_type == "epsilon":
+            sqrt_alphas_prod = jnp.sqrt(self.alphas_cumprod[t])
+            sqrt_one_minus_alphas_prod = jnp.sqrt(1 - self.alphas_cumprod[t])
+            # noised_sample = sqrt_alphas_prod * denoised + sqrt_one_minus_alphas_prod * noise
+            # therefore noise = (sample - sqrt_alphas_prod * denoised) / sqrt_one_minus_alphas_prod
+            nosied_sample_flat, unflatten = jax.flatten_util.ravel_pytree(noised_sample)
+            denoised_sample_flat, _ = jax.flatten_util.ravel_pytree(denoised_sample)
+            noise = (nosied_sample_flat - sqrt_alphas_prod * denoised_sample_flat) / sqrt_one_minus_alphas_prod
+            return unflatten(noise)
+    
+    @jax.jit
+    def compute_denoised(self, noised_sample, t, data_batch):
+        noised_sample_flat, unflatten = jax.flatten_util.ravel_pytree(noised_sample)
+        data_batch_flat = jax.vmap(lambda x: jax.flatten_util.ravel_pytree(x)[0])(data_batch)
+        # compute the mean
+        sqrt_alphas_prod = jnp.sqrt(self.alphas_cumprod[t])
+        sqrt_one_minus_alphas_prod = jnp.sqrt(1 - self.alphas_cumprod[t])
+
+        # noised_sample = sqrt_alphas_prod * denoised + sqrt_one_minus_alphas_prod * noise
+        noise = (noised_sample_flat[None,:] - sqrt_alphas_prod * data_batch_flat) / sqrt_one_minus_alphas_prod
+        # the z^2 term for how far the noised_sample is from the data_batch
+        z_sqr = jnp.sum(noise**2, axis=-1)
+        log_likelihood = -0.5*z_sqr
+        log_likelihood = log_likelihood - jax.scipy.special.logsumexp(log_likelihood, axis=0)
+        # this is equivalent to the log-likelihood (up to a constant factor)
+        denoised = jnp.sum(jnp.exp(log_likelihood)[:,None]*data_batch_flat, axis=0)
+        return denoised
+
     # This does a reverse process step
     @jax.jit
-    def step(self, rng_key, sample, timestep, delta_steps, model_output):
+    def reverse_step(self, rng_key, sample, timestep, delta_steps, model_output):
         #chex.assert_trees_all_equal_shapes_and_dtypes(sample, model_output)
         t = timestep
         prev_t = t - delta_steps
         alpha_prod_t = self.alphas_cumprod[t]
-        alpha_prod_t_prev = jax.lax.cond(prev_t >= 0, lambda: self.alphas_cumprod[prev_t],
-                                        lambda: jnp.ones(()))
-
+        alpha_prod_t_prev = self.alphas_cumprod_prev[prev_t + 1]
         sample_flat, unflatten = jax.flatten_util.ravel_pytree(sample)
         model_output_flat, _ = jax.flatten_util.ravel_pytree(model_output)
 
@@ -117,15 +169,7 @@ class DDPMSchedule:
         current_alpha_t = alpha_prod_t / alpha_prod_t_prev
         current_beta_t = 1 - current_alpha_t
 
-        if self.prediction_type == "epsilon":
-            pred_sample = (sample_flat - beta_prod_t ** (0.5) * model_output_flat) / alpha_prod_t ** (0.5)
-        elif self.prediction_type == "sample":
-            pred_sample = model_output_flat
-        elif self.prediction_type == "v_prediction":
-            pred_sample = (alpha_prod_t**0.5) * sample_flat - (beta_prod_t**0.5) * model_output_flat
-
-        if self.clip_sample_range is not None:
-            pred_sample = jnp.clip(pred_sample, -self.clip_sample_range, self.clip_sample_range)
+        pred_sample = self.denoised_from_output(sample_flat, t, model_output_flat)
 
         pred_original_sample_coeff = jnp.sqrt(alpha_prod_t_prev) * current_beta_t / beta_prod_t
         current_sample_coeff = jnp.sqrt(current_alpha_t) * beta_prod_t_prev / beta_prod_t
@@ -145,7 +189,7 @@ class DDPMSchedule:
         with jax.named_scope("eval_model"):
             model_output = model(model_rng, sample, timestep)
         with jax.named_scope("step"):
-            next_sample = self.step(step_rng, sample, timestep,
+            next_sample = self.reverse_step(step_rng, sample, timestep,
                                     delta_steps, model_output)
         out = next_sample if trajectory else None
         return (rng_key, next_sample), out
