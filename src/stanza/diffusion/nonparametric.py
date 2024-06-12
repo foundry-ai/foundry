5a968dbd8b5e64d36876613756dfc1030111a025
Christina Yu <chryu@dhcp-10-29-196-219.dyn.MIT.EDU>
2024-06-11 22:21:42 -0400

    preliminary local polynomial estimator

--- src/stanza/diffusion/nonparametric.py
+++ src/stanza/diffusion/nonparametric.py
@@ -56,3 +56,108 @@ def nw_cond_diffuser(cond, data, schedule, kernel, h):
         estimator = nadaraya_watson(estimator_data, comb_kernel, h)
         return estimator((cond, noised_value))
     return diffuser
+
+def expand_poly_features(degree, samples):
+    # Adapted from sklearn.preprocessing.PolynomialFeatures
+    num_features = samples.shape[-1]
+    num_poly_features = binom(degree + samples.shape[-1], samples.shape[-1]).astype(int)
+    poly_samples = jnp.empty((samples.shape[0], num_poly_features))
+    poly_samples = poly_samples.at[:, 0].set(1)
+    if degree >= 1:
+        poly_samples = poly_samples.at[:, 1:1+num_features].set(samples)
+    if degree >= 2:
+        index = list(range(1, 1+num_features))
+        current_col = 1 + num_features 
+        index.append(current_col)
+        for _ in range(2, degree + 1):
+            new_index = []
+            end = index[-1]
+            for feature_idx in range(num_features):
+                start = index[feature_idx]
+                new_index.append(current_col)
+                next_col = current_col + end - start
+                if next_col <= current_col:
+                    break
+                # XP[:, start:end] are terms of degree d - 1
+                # that exclude feature #feature_idx.
+                poly_samples = poly_samples.at[:, current_col:next_col].set(
+                    poly_samples[:, start:end] * samples[:, feature_idx : feature_idx + 1]
+                )
+                current_col = next_col
+
+            new_index.append(current_col)
+            index = new_index
+    return poly_samples
+
+def nw_local_poly(rng_key, data, schedule, degree, cond_kernel, noised_value_kernel, h_cond, h_noised, num_noised_samples):
+
+
+    def estimator(cond, noised_value, t):
+
+        xs, ys = data
+
+        # Flatten the data
+        vf = jax.vmap(lambda x: jax.flatten_util.ravel_pytree(x)[0])
+
+        x0 = jax.tree_map(lambda x: x[0], xs)
+        xs = vf(xs)
+        _, x_uf = jax.flatten_util.ravel_pytree(x0)
+
+        y0 = jax.tree_map(lambda x: x[0], ys)
+        ys = vf(ys)
+        _, y_uf = jax.flatten_util.ravel_pytree(y0)
+
+        # Flatten input
+        cond, cond_uf = jax.flatten_util.ravel_pytree(cond)
+        noised_value, noised_value_uf = jax.flatten_util.ravel_pytree(noised_value)
+
+        add_noise = lambda rng, x, y, t : (x, y, schedule.add_noise(rng, y, t)[0])
+        batch_add_noise = lambda rng, x, y, t: jax.vmap(add_noise, in_axes=(0, None, None, None))(
+            jax.random.split(rng_key, num_noised_samples), x, y, t
+        ) 
+        noised_samples = jax.vmap(batch_add_noise, in_axes=(0,0,0,None))(
+            jax.random.split(rng_key, xs.shape[0]),
+            xs,
+            ys,
+            t
+        )
+        xs = jnp.reshape(noised_samples[0], (-1, xs.shape[-1]))
+        ys = jnp.reshape(noised_samples[1], (-1, ys.shape[-1]))
+        yts = jnp.reshape(noised_samples[2], (-1, ys.shape[-1]))
+
+        zs = jnp.concatenate((xs, yts), axis=-1)
+        z = jnp.concatenate((cond, noised_value), axis=-1)
+        
+        cond_K = lambda x, xi: cond_kernel((x-xi)/h_cond)
+        noised_value_K = lambda x, xi: noised_value_kernel((x-xi)/h_noised)
+        
+        log_prob = (
+            jax.vmap(cond_K, in_axes=(None, 0))(cond, xs) + 
+            jax.vmap(noised_value_K, in_axes=(None, 0))(noised_value, yts)
+        )
+        prob = jnp.exp(log_prob)[:,None]
+
+        poly_zs = expand_poly_features(degree, zs)
+        poly_z = expand_poly_features(degree, z[None, :])
+
+        M = poly_zs.T @ (prob * poly_zs)
+        b = poly_zs.T @ (prob * ys)
+        coeffs, _, _, _ = jnp.linalg.lstsq(M, b)
+
+        y_est = jnp.dot(poly_z, coeffs)
+        jax.debug.print('{s}', s=jax.vmap(cond_K, in_axes=(None, 0))(cond, xs))
+        jax.debug.print('{s}', s=jax.vmap(noised_value_K, in_axes=(None, 0))(noised_value, yts))
+        
+        return noised_value_uf(y_est)
+    
+    return estimator
+
+def nw_local_poly_diffuser(cond, estimator):
+    @jax.jit
+    def diffuser(_, noised_value, t):
+        return estimator(cond, noised_value, t)
+    return diffuser
+
+
+
+        
