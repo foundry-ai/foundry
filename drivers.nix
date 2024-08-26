6a2278b50e5eff616ee0b3440a186c5092aee168
Daniel Pfrommer <dan.pfrommer@gmail.com>
2024-08-26 15:30:58 -0400

    Updated make-driver-config

--- drivers.nix
+++ drivers.nix
@@ -5,13 +5,22 @@ lib = nixpkgs.lib;
 driverConfig = lib.importJSON ./.drivers/config.json;
 
     cudaLib = ./.drivers/${driverConfig.cuda.path};
-    cudaDrivers = nixpkgs.runCommand "cuda-drivers" {} (''
-        mkdir -p $out $out/lib
-        cp ${cudaLib} $out/lib/libcuda.so
-        ln -s $out/lib/libcuda.so $out/lib/libcuda.so.1
-    '');
+    cudaDrivers = lib.optionals (builtins.hasAttr "cuda" driverConfig) [
+            nixpkgs.runCommand "cuda-drivers" {} (''
+            mkdir -p $out $out
+            cp ${cudaLib} $out/lib/libcuda.so
+            ln -s $out/lib/libcuda.so $out/lib/libcuda.so.1
+        '')
+    ];
+    glDrivers = builtins.map (
+        driver: nixpkgs.runCommand "driver-${driver.name}"
+    ) driverConfig.drivers;
 
+drivers = [];
+driverLibPaths = lib.makeLibraryPath drivers;
+vendorPaths = lib.strings.concatStringsSep ":" (map (driver: "${driver}/vendor.json") glDrivers)
 in
 ''
-    export LD_LIBRARY_PATH=${cudaDrivers}/lib
+    export LD_LIBRARY_PATH=${driverLibPaths}
+    export __EGL_VENDOR_LIBRARY_FILENAMES=${vendorPaths}
 ''
