15de9a819b8537c6f061e466e5bd8169ed5b9fa1
Daniel Pfrommer <dan.pfrommer@gmail.com>
2024-08-26 16:17:38 -0400

    Cuda drivers working, working on gl version

--- drivers.nix
+++ drivers.nix
@@ -3,22 +3,29 @@ let
 
 lib = nixpkgs.lib;
 driverConfig = lib.importJSON ./.drivers/config.json;
+cudaPath = ./.drivers/${driverConfig.cuda.path};
+cudaDrivers = lib.optionals (builtins.hasAttr "cuda" driverConfig) [
+    (nixpkgs.runCommand "cuda-drivers" {} (''
+        mkdir -p $out
+        cp -r ${cudaPath} $out/lib
+    ''))
+];
+glDrivers = builtins.map (
+    driver:  let 
+        driverPath = ./.drivers/${driver.path};
+    in
+    nixpkgs.runCommand "${driver.name}-driver" {} (''
+        mkdir -p $out
+        cp -r ${driverPath} $out/lib
+    '')
+) driverConfig.drivers;
 
-    cudaLib = ./.drivers/${driverConfig.cuda.path};
-    cudaDrivers = lib.optionals (builtins.hasAttr "cuda" driverConfig) [
-            nixpkgs.runCommand "cuda-drivers" {} (''
-            mkdir -p $out $out
-            cp ${cudaLib} $out/lib/libcuda.so
-            ln -s $out/lib/libcuda.so $out/lib/libcuda.so.1
-        '')
-    ];
-    glDrivers = builtins.map (
-        driver: nixpkgs.runCommand "driver-${driver.name}"
-    ) driverConfig.drivers;
-
-drivers = [];
+drivers = cudaDrivers ++ glDrivers;
 driverLibPaths = lib.makeLibraryPath drivers;
-vendorPaths = lib.strings.concatStringsSep ":" (map (driver: "${driver}/vendor.json") glDrivers)
+
+vendorPaths = lib.strings.concatStringsSep ":" (
+    map (driver: "${driver}/egl/vendor.json") glDrivers
+);
 in
 ''
     export LD_LIBRARY_PATH=${driverLibPaths}
